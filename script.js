b567e4b3923dea7f9d9bdd0fbf6dd2137992d4e0
Kings <kingsley.okechukwu@outlook.com>
2024-09-28 21:30:50 +0100

    First task

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('registration-form');
     const feedbackDiv = document.getElementById('form-feedback');
 
     
 
-    form.addEventListener('submit', (event) => {
+    form.addEventListener('submit', function (event) {
         event.preventDefault();
         const usernameInput = document.getElementById('username').value.trim();
     const emailInput = document.getElementById('email').value.trim();
